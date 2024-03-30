68571bf3931d7b0d8161bdb16617fab45a69e365
Thushan W A R T <rusiththarindu2@gmail.com>
2024-03-30 21:41:10 +0530

    branch init

--- frontend/air-quality/App.js
+++ frontend/air-quality/App.js
@@ -1,18 +1,31 @@
-
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import RealTimeData from './src/RealTimeData';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 
-export default function App() {
+const HomeScreen = () => {
   return (
-    <View>
-      <RealTimeData />
-    
-    </View>
+    <>
+      <RealTimeData/>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
-});
+export default App;
