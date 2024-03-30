888a9177074a23332e4707f028fa5dcdaa033dce
Thushan W A R T <rusiththarindu2@gmail.com>
2024-03-30 23:28:05 +0530

    Navigation Added

--- frontend/air-quality/src/RealTimeData.jsx
+++ frontend/air-quality/src/RealTimeData.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, Dimensions, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native';
 import { getDatabase, ref, onValue } from 'firebase/database';
 import { db } from '../config'; 
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
 import {
   LineChart,
   BarChart,
@@ -70,17 +70,24 @@ const RealTimeData = () => {
   return (
     <SafeAreaView 
       style = {{ justifyContent : 'start', alignItems : 'center', width : '100%', height : '100%', backgroundColor : '#071414',}}
-      
       >
   <StatusBar hidden = {false} backgroundColor='gray'/>
 
       <ScrollView style = {{paddingTop : 30}}>
 
+        <View style ={{width : Dimensions.get("window").width,height : 'auto', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          <Text style = {{color: 'white', fontSize: 25, marginTop: 10, fontWeight: 600 }}>Welcome to AuraSense</Text>
+          <Text style = {{color: 'gray', fontSize: 12 }}>Scroll Down to view Real-Time Data</Text>
+          <Text style = {{color: 'gray', fontSize: 12, marginBottom: 10  }}>Click on "See More" for Analytics</Text>
+        </View>
+
       <View style = {{width : Dimensions.get("window").width,height : 'auto'}}>
-          <Text style = {{color:'white',fontSize : 25,paddingLeft : 30, marginTop: 10}}>Humidity</Text>
-          <TouchableOpacity onPress={() => {}}>
-            <Text>See More</Text>
-          </TouchableOpacity>
+          <View style = {{height: 'auto', width : Dimensions.get("window").width, display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems:'center'}}>
+            <Text style = {{color:'white',fontSize : 25,paddingLeft : 30, marginTop: 10}}>Humidity</Text>
+            <TouchableOpacity onPress={() => {}}>
+              <Text style = {{paddingRight : 30, paddingTop: 10, color: 'white', }}>See More</Text>
+            </TouchableOpacity>
+          </View>
           <View style = {{justifyContent : 'center',alignItems : 'center'}}>
             
           <Text style = {{color:'white',position : 'absolute'}}>{data.Humidity + '%'}</Text>
@@ -111,10 +118,15 @@ const RealTimeData = () => {
 
 
       <View style = {{width : Dimensions.get("window").width,height : 'auto'}}>
-        <Text style = {{color:'white',fontSize : 25,paddingLeft : 30}}>Temperature</Text>
+      <View style = {{height: 'auto', width : Dimensions.get("window").width, display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems:'center'}}>
+            <Text style = {{color:'white',fontSize : 25,paddingLeft : 30, marginTop: 10}}>Temperature</Text>
+            <TouchableOpacity onPress={() => {}}>
+              <Text style = {{paddingRight : 30, paddingTop: 10, color: 'white', }}>See More</Text>
+            </TouchableOpacity>
+          </View>
         
       <View style = {{justifyContent : 'center',alignItems : 'center'}}>
-      <Text style = {{color:'white',position : 'absolute'}}>{data.Temperature + '%'}</Text> 
+      <Text style = {{color:'white',position : 'absolute'}}>{data.Temperature + ' C'}</Text> 
           <ProgressChart
           data={data2}
           width={Dimensions.get("window").width-50}
@@ -141,7 +153,12 @@ const RealTimeData = () => {
 
 
       <View style = {{width : Dimensions.get("window").width,height : 'auto'}}>
-      <Text style = {{color:'white',fontSize : 25,paddingLeft : 30}}>Harmful Gases</Text>
+      <View style = {{height: 'auto', width : Dimensions.get("window").width, display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems:'center'}}>
+            <Text style = {{color:'white',fontSize : 25,paddingLeft : 30, marginTop: 10}}>Harmful Gases</Text>
+            <TouchableOpacity onPress={() => {}}>
+              <Text style = {{paddingRight : 30, paddingTop: 10, color: 'white', }}>See More</Text>
+            </TouchableOpacity>
+          </View>
       <View style = {{justifyContent : 'center',alignItems : 'center'}}>
       <Text style = {{color:'white',position : 'absolute'}}>{data.Gas /100}</Text>
         <ProgressChart
